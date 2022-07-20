6016c509f59a5bafd336897f5cb336704b6f5c6f
H4 <67812277+HELLLO1073@users.noreply.github.com>
2022-07-20 23:00:09 +1000

    Set up

---
+++ Games/Anomic/Anomiss.lua
@@ -0,0 +1,13 @@
+-- Set up
+
+-- // Kill function 
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/Anomiss/main/Games/Anomic/Obfuscation/kk.lua"))()
+
+-- Auto heal
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/Anomiss/main/Games/Anomic/Obfuscation/autoheal.lua"))()
+
+-- Anticar
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/Anomiss/main/Games/Anomic/Dependencies/Anti-car.lua"))()
+
+-- Main hook 
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/Anomiss/main/Games/Anomic/Dependencies/hook.lua"))()
