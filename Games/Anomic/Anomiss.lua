74f69006dcc038ba9c85eaa245dc338f55eaacfd
H4 <67812277+HELLLO1073@users.noreply.github.com>
2022-07-21 19:32:54 +1000

    Update Anomiss.lua

--- Games/Anomic/Anomiss.lua
+++ Games/Anomic/Anomiss.lua
@@ -1,13 +1,3 @@
--- Set up for obfuscation & release
+--// Anomic Original | Anomiss beta
+--// Script creators [H4#0321 & Bonfire#9355]
 
--- // Kill function 
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/Anomiss/main/Games/Anomic/Obfuscation/kk.lua"))()
-
--- Auto heal
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/Anomiss/main/Games/Anomic/Obfuscation/autoheal.lua"))()
-
--- Anticar
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/Anomiss/main/Games/Anomic/Dependencies/Anti-car.lua"))()
-
--- Main hook 
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/Anomiss/main/Games/Anomic/Dependencies/hook.lua"))()
