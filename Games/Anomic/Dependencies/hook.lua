df3ef92a0fd6a676a4cf502c2ee066b6a0a034cf
H4 <67812277+HELLLO1073@users.noreply.github.com>
2022-09-11 16:05:30 +1000

    Update hook.lua

--- Games/Anomic/Dependencies/hook.lua
+++ Games/Anomic/Dependencies/hook.lua
@@ -1,72 +1,14 @@
-local LocalPlayer = game.Players.LocalPlayer
-local CCamera = workspace.CurrentCamera
-
-local Settings = {
-    EndColor = Color3.new(1, 1, 1),
-    StartWidth = 0.1,
-    EndWidth = 0.05,    
-    Time = 1
-}
+_G.BeamSettings = {Enabled = true, StartColor = Color3.new(0.882352, 0, 1),EndColor = Color3.new(1, 1, 1),StartWidth = 0.1,EndWidth = 0.05,Time = 1}
+_G.AimSettings = {Enabled = false, Checks = {TeleBullet = true}}
+_G.AimTarget = {Part = nil}
 
 local BeamPart = Instance.new("Part", workspace)    
 BeamPart.Name = "BeamPart"
 BeamPart.Transparency = 1
 
-local function Beam(origin, end_part)    
-    if _G.beams.Enabled then
-        
-        local colorSequence = ColorSequence.new({ColorSequenceKeypoint.new(0, _G.beams.Color),ColorSequenceKeypoint.new(1, Settings.EndColor),})
-        local Part = Instance.new("Part", BeamPart)
-
-        Part.Size = Vector3.new(1, 1, 1)
-        Part.Transparency = 1
-        Part.CanCollide = false
-        Part.CFrame = CFrame.new(origin)
-        Part.Anchored = true
-
-        local Attachment = Instance.new("Attachment", Part)        
-
-        local Attachment2 = Instance.new("Attachment", end_part)
-        local Beam = Instance.new("Beam", Part)
-
-        Beam.FaceCamera = true
-        Beam.Color = colorSequence
-        Beam.Attachment0 = Attachment
-        Beam.Attachment1 = Attachment2
-        Beam.LightEmission = 6
-        Beam.LightInfluence = 1
-        Beam.Width0 = Settings.StartWidth
-        Beam.Width1 = Settings.EndWidth  
-
-        delay(Settings.Time, function()        
-            for i = 0.5, 1, 0.02 do
-                wait()
-                Beam.Transparency = NumberSequence.new(i)
-            end
-            Part:Destroy()
-            Attachment2:Destroy()
-        end)
-
-    end
-end   
-
-local function CorrectArguments(Args)
-    local Matching = 0
-    local Required = 2
-    local TypeList = {"Instance", "Ray", "Instance", "boolean", "boolean"}
-
-    if #Args < Required then
-        return false    
-    end
-
-    for i, a in next, Args do
-        if typeof(a) == TypeList[i] then
-            Matching = Matching + 1
-        end
-    end   
-
-    return Matching >= Required
-end
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local CCamera = workspace.CurrentCamera
 
 local function HitDetection(character, part)
     if _G.hitmanager.hitsounds.Enabled and character then
@@ -85,40 +27,64 @@ local function HitDetection(character, part)
     end
 end
 
-local RayCastLength = 4500
+local function Beam(origin, end_part)        
+    if _G.BeamSettings.Enabled then
+        local colorSequence = ColorSequence.new({ColorSequenceKeypoint.new(0, Color3.fromRGB(225, 0, 255)),ColorSequenceKeypoint.new(1, _G.BeamSettings.EndColor),})
+        local Part = Instance.new("Part", BeamPart)
+
+        Part.Size = Vector3.new(1, 1, 1)
+        Part.Transparency = 1
+        Part.CanCollide = false
+        Part.CFrame = CFrame.new(origin)
+        Part.Anchored = true
+
+        local Attachment = Instance.new("Attachment", Part)  
+        local Attachment2 = Instance.new("Attachment", end_part)
+        local Beam = Instance.new("Beam", Part)
+
+        Beam.FaceCamera = true
+        Beam.Color = colorSequence
+        Beam.Attachment0 = Attachment
+        Beam.Attachment1 = Attachment2
+        Beam.LightEmission = 6
+        Beam.LightInfluence = 0
+        Beam.Width0 = _G.BeamSettings.StartWidth
+        Beam.Width1 = _G.BeamSettings.EndWidth  
+
+        delay(_G.BeamSettings.Time, function()        
+            for i = 0.5, 1, 0.02 do
+                wait()
+                Beam.Transparency = NumberSequence.new(0)
+            end
+            Part:Destroy()
+            Attachment2:Destroy()
+        end)
+    end
+end
+
 local oldNamecall, oldIndex
 oldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(...)
     local Method = getnamecallmethod()
     local CallingScript = getcallingscript()
     local Arguments = {...}
-    local self = Arguments[1]
+    local self = Arguments[1]  
 
-    local ray_origin = nil       
-
-    if _G.silent.Enabled and tostring(self) == "Workspace" then 
-
-        if _G.silent.hitpart ~= nil and LocalPlayer.Character and game.FindFirstChild(LocalPlayer.Character, "Head") then                 
-
-            if _G.silent.origin == "Camera" then
-                ray_origin = CCamera.CFrame.Position
-            elseif _G.silent.origin == "MyHead" then
-                ray_origin = game.FindFirstChild(LocalPlayer.Character, "Head").Position                              
-            elseif _G.silent.origin == "Teleport" then
-                ray_origin = _G.silent.hitpart.Position + Vector3.new(0,1,0)               
-            end
-
-            if tostring(Method) == "FindPartOnRay" and CorrectArguments(Arguments) then
+    if _G.AimSettings.Enabled and tostring(self) == "Workspace" then 
+        if LocalPlayer.Character and game.FindFirstChild(LocalPlayer.Character, "Head") and _G.AimTarget and _G.AimTarget.Part then       
+            if tostring(Method) == "FindPartOnRay" and tostring(CallingScript) == "MainGunScript"  then
                 local CurrentRay = Arguments[2] 
                 local origin = CurrentRay.Origin              
-
-                if _G.silent.origin == "Called" then
-                    Arguments[2] = Ray.new(origin, (_G.silent.hitpart.Position-origin).Unit * RayCastLength)                
-                    task.spawn(Beam, origin, _G.silent.hitpart) 
+                
+                if _G.AimSettings.Checks.TeleBullet then
+                    local randpoints = {Vector3.new(0.7, 0, 0), Vector3.new(0, 0, 0.7)}
+                    local tpoint = _G.AimTarget.Part.Position + randpoints[math.random(1, #randpoints)]                       
+                    
+                    task.spawn(Beam, origin, _G.AimTarget.Part)                           
+                    Arguments[2] = Ray.new(tpoint, (_G.AimTarget.Part.Position-tpoint).Unit * 5000)    
                 else
-                    Arguments[2] = Ray.new(ray_origin, (_G.silent.hitpart.Position-ray_origin).Unit * RayCastLength)            
-                    task.spawn(Beam, ray_origin, _G.silent.hitpart)     
-                end             
-
+                    Arguments[2] = Ray.new(origin, (_G.AimTarget.Part.Position-origin).Unit * 5000)
+                end
+               
                 return oldNamecall(unpack(Arguments))   
             end 
         end
@@ -129,13 +95,9 @@ oldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(...)
         return
     end
 
-    if _G.flightshot and tostring(Method) == "FindPartOnRayWithIgnoreList" and CallingScript.Name == "MainGunScript" then
+    if tostring(Method) == "FindPartOnRayWithIgnoreList" and CallingScript.Name == "MainGunScript" then
         return true
     end
-
-    if Method == "Kick" then		
-	    return nil                    
-    end 
 
     if tostring(Method) == "FireServer" and tostring(self) == "WeaponServer" and tostring(Arguments[2]) == "Player" then
         local character = Arguments[3].Parent
@@ -150,6 +112,10 @@ oldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(...)
             return oldNamecall(unpack(Arguments))
         end
     end
+
+    if Method == "Kick" then		
+	    return nil                    
+    end    
     
     return oldNamecall(...)
 end))
