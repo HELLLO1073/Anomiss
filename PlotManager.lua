9c8bbbe99ab58124480eaf0d55f024be83607054
H4 <67812277+HELLLO1073@users.noreply.github.com>
2024-09-04 11:30:53 +1000

    Update PlotManager.lua

--- PlotManager.lua
+++ PlotManager.lua
@@ -1,3 +1,2 @@
+-- Obfuscated and secured to prevent skidding, credits to h4 & bonfire
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9a340d909826f78c7891732f574000c8.lua"))()
-
--- Credits to h4 & bonfire
